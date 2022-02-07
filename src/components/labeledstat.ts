f16f8a38ca3253730fd63d0cda53a7e0140c557e
Makarand Patwardhan <mpatwardhan@twilio.com>
2022-02-07 10:16:23 -0800

    use 2.19.1

--- src/components/labeledstat.ts
+++ src/components/labeledstat.ts
@@ -4,6 +4,20 @@ import jss from '../jss'
 
 // Create your style.
 const style = {
+  containerOverFlowHidden: {
+    overflow: 'hidden',
+  },
+  '@keyframes blink': {
+    '0%': 'transform:scale(1)',
+    '50%': 'transform:scale(1.2)',
+    '100%': 'transform:scale(1)',
+  },
+  runBlinkAnimation: {
+    animation: '$blink 200ms',
+  },
+  runShowHideAnimation: {
+    animation: '$showHide 2000ms',
+  },
   labeledStat: {
     background: 'solid green 2px',
     margin: '2px'
@@ -38,13 +52,32 @@ export function createLabeledStat({ container, label, id, valueMapper } : {
         el.classList.remove(lastClass);
       }
 
-      el.textContent = label + ': ' + text;
+      const oldText = el.textContent;
+      const newText = label + ': ' + text;
+      el.textContent = newText;
 
       if (valueMapper) {
         lastClass = valueMapper(text);
         if (lastClass) {
           el.classList.add(lastClass);
         }
+      }
+      if (oldText !== newText) {
+        // run animation as text changes.
+        // to avoid parent resizing from scale animation
+        // markit overflow: hidden while animation is running
+        let containerNeedsUpdate = !container.classList.contains(sheet.classes.containerOverFlowHidden);
+        if (containerNeedsUpdate) {
+          container.classList.add(sheet.classes.containerOverFlowHidden);
+        }
+
+        el.classList.add(sheet.classes.runBlinkAnimation);
+        el.addEventListener('animationend', () => {
+          el.classList.remove(sheet.classes.runBlinkAnimation);
+          if (containerNeedsUpdate) {
+            container.classList.remove(sheet.classes.containerOverFlowHidden);
+          }
+        });
       }
     }
   };
